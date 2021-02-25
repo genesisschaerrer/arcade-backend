1f6c6a4f083bdd9bd5297bed49e042bbd7b95e09
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-02-25 13:42:50 -0700

    editing headers

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.json())
 app.use(cors({
     origin: ["https://arcade-client.herokuapp.com", "http://localhost:3000", "https://nostalgic-brattain-51b8a7.netlify.app/"],
     allowedHeaders: ["Access-Control-Allow-Origin", "Content-Type", "Cookies"],
+    exposedHeaders: "token",
     // AccessControlAllowOrigin: ["https://arcade-client.herokuapp.com", "http://localhost:3000", "https://nostalgic-brattain-51b8a7.netlify.app/"],
     credentials: true
 }))
